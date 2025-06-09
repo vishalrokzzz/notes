16162c3b7a0ed58f4b98a7af2b5860a42c56d7c6
vishalrokzzz <vishaldasari696@gmail.com>
2025-06-09 15:35:31 +0530

    created react app with vite,setup browser-router and react-hot-toast,created pages,setup tailwind and daisyUI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Route,Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import CreatePage from './pages/CreatePage';
+import NoteDetailPage from './pages/NoteDetailPage';
+import toast from 'react-hot-toast';
+
+const App = () => {
+  return (
+    <div data-theme="lemonade">
+        <button className='btn btn-success' >click here</button>
+        
+    
+        <Routes>
+            <Route path="/" element={<HomePage/>}/>
+            <Route path="/create" element={<CreatePage/>}/>
+            <Route path="/note/:id" element={<NoteDetailPage/>}/>
+
+        </Routes>
+    </div>
+  )
+};
+
+export default App
