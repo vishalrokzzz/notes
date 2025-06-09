f5a8e16a6767531017fd5e82cf8612747bfe558b
vishalrokzzz <vishaldasari696@gmail.com>
2025-06-09 20:05:09 +0530

    added loading indication in ui logic,printing the fetched notes to ui

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,48 +1,59 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from '../components/Navbar';
-import RateLimitedUI from '../components/RateLimitedUI';
-import axios from 'axios';
-import toast from 'react-hot-toast';
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import RateLimitedUI from "../components/RateLimitedUI";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const HomePage = () => {
-    const [isRateLimited,setRateLimited]=useState(false);
-    const [notes,setNotes]=useState([]);
-    const [loading,setLoading]= useState(true);
+  const [isRateLimited, setRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get("http://localhost:5001/api/notes");
+        console.log(res.data);
+        setNotes(res.data);
+        setRateLimited(false);
+      } catch (error) {
+        console.log("error fetching notes");
+        console.log(error);
+        if (error.response?.status === 429) {
+          setRateLimited(true);
+        } else {
+          toast.error("failed to fetch notes");
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
 
-    useEffect(() => {
-        const fetchNotes= async () =>{
-            try {
-                const res = await axios.get("http://localhost:5001/api/notes");                
-                console.log(res.data);
-                setNotes(res.data);
-                setRateLimited(false);
-                
-            } catch (error) {
-                console.log("error fetching notes");
-                console.log(error);
-                if (error.response?.status === 429){
-                    setRateLimited(true);
-
-                }else{
-                    toast.error("failed to fetch notes");
-                    
-                }
-                
-            }
-            finally{
-                setLoading(false); 
-            }
-        };
-
-        fetchNotes();
-    },[])
+    fetchNotes();
+  }, []);
   return (
-    <div className='min-h-screen'>
-        <Navbar/>
-        {isRateLimited && <RateLimitedUI/>}
+    <div className="min-h-screen">
+      <Navbar />
+      {isRateLimited && <RateLimitedUI />}
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && <div className="text-center text-primary py-10"> Loading notes...</div>  }
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map(note => (
+              <div>
+                {note.title} | {note.content}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+
+      
+
+
+
     </div>
-  )
+  );
 };
 
 export default HomePage;
